931700cb3f6e6f249f9f811c5d54be1f79c954ae
Joe Cicchiello <j.cicchiello@ieee.org>
2017-10-19 11:28:25 -0400

    bug fix in compressd.py: establishing symlink and moving files around after compression was crashing; also added top-level exception trap to send email

--- py/compressd.py
+++ py/compressd.py
@@ -149,8 +149,9 @@ def closeCompression(n, now, fs):
         print "subprocess.CalledProcessError:",e.output
         exit()
     
-    dstfile = fs+'/library/'+cleanDescription(id)+'.mp4';
+    dstfile = fs+'/library/'+cleanDescription(n['description'])+'.mp4';
     print nowstr(),"DEBUG: Here's the symlink to establish:",outfile," ",dstfile
+    os.remove(dstfile)
     os.symlink(outfile, dstfile)
     
     #mv raw file to ./trashcan
@@ -272,7 +273,25 @@ def zombieHunt(now):
             revertCompression(n, now, dvr_fs)
 
 
-    
+
+def sysexception():
+    f = open("/tmp/trap.txt", "w", 0)
+    f.write("To: j.cicchiello@ieee.org\n")
+    f.write("From: jcicchiello@ptd.net\n")
+    f.write("Subject: compressd.py has crashed!?!?\n")
+    f.write("\n")
+    f.write("compressd.py has shutdown unexpectedly!\n");
+    f.write("\n")
+    f.close()
+    with open('/tmp/msg.txt', 'r') as infile:
+        subprocess.Popen(['/usr/sbin/ssmtp', 'j.cicchiello@gmail.com'],
+                         stdin=infile, stdout=sys.stdout, stderr=sys.stderr)
+    exit()
+
+
+sys.excepthook = sysexception
+
+
 urs = getUncompressedRecordingSet(None)
 now = calendar.timegm(time.gmtime())
 zombieTimestamp = now
